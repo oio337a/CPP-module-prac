8c7b66862c5f7b464ab6f0a3ae4289485e16242b
Yongmin Park <yongmipa@student.42seoul.kr>
2023-05-24 20:47:54 +0900

    ex00 clear

---
+++ cpp_module_00/ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int	main(int ac, char *av[])
+{
+	std::string	str;
+
+	if (ac == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	for (int i = 1; i < ac; ++i) {
+		str = av[i];
+		for (std::string::iterator it = str.begin(); it != str.end(); ++it) {
+			std::islower(*it) ? std::cout << static_cast<char>(std::toupper(*it)) : std::cout << *it;
+		}
+	}
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
+}
