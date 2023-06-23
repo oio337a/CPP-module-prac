bd7fefe68ccddf65e0ca584b12dcc53817e200de
oio337a <yongmipa@student.42seoul.kr>
2023-06-23 22:16:01 +0900

    ex03 작성중

--- cpp_module_01/ex02/main.cpp
+++ cpp_module_01/ex02/main.cpp
@@ -2,10 +2,9 @@
 
 int main(void)
 {
-	std::string str =  "HI THIS IS BRAIN";
+	std::string		str =  "HI THIS IS BRAIN";
 	std::string*	stringPTR = &str;
 	std::string&	stringREF = str;
-
 
 	std::cout << "=============================="<< std::endl;
 
@@ -18,5 +17,4 @@ int main(void)
 	std::cout << "string: " << str << std::endl;
 	std::cout << "stringPTR: " << *stringPTR << std::endl;
 	std::cout << "stringREF: " << stringREF << std::endl;
-
 }
