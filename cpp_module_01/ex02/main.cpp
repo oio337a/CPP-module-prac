0fe48c5a5cca6557055195cf3240b57f9a57ff6f
oio337a <yongmipa@student.42seoul.kr>
2023-06-23 21:39:00 +0900

    ex02 clear

---
+++ cpp_module_01/ex02/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(void)
+{
+	std::string str =  "HI THIS IS BRAIN";
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
+
+
+	std::cout << "=============================="<< std::endl;
+
+	std::cout << "string 주소: " << &str << std::endl;
+	std::cout << "stringPTR 주소: " << &stringPTR << std::endl;
+	std::cout << "stringREF 주소: " << &stringREF << std::endl;
+
+	std::cout << "=============================="<< std::endl;
+
+	std::cout << "string: " << str << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+
+}
