dabcda836939b1ade889abd5590eebdf24dd2bf9
oio337a <yongmipa@student.42seoul.kr>
2023-06-24 16:09:26 +0900

    style: position

--- cpp_module_01/ex02/main.cpp
+++ cpp_module_01/ex02/main.cpp
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-	std::string		str =  "HI THIS IS BRAIN";
-	std::string*	stringPTR = &str;
-	std::string&	stringREF = str;
+	std::string	str =  "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
 
 	std::cout << "=============================="<< std::endl;
 
