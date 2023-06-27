4c49e6116574b5376835ae65231835c7eb6c7c54
oio337a <yongmipa@student.42seoul.kr>
2023-06-27 18:00:13 +0900

    ex05 initial

---
+++ cpp_module_01/ex05/Karen.cpp
@@ -0,0 +1,29 @@
+#include "Karen.hpp"
+
+Karen::Karen(/* args */)
+{
+}
+
+Karen::~Karen()
+{
+}
+
+void Karen::complain(std::string level)
+{
+}
+
+void Karen::debug(void)
+{
+}
+
+void Karen::info(void)
+{
+}
+
+void Karen::warning(void)
+{
+}
+
+void Karen::error(void)
+{
+}
---
+++ cpp_module_01/ex05/Karen.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+class Karen
+{
+private:
+	/* data */
+	void debug( void );
+	void info( void );
+	void warning( void );
+	void error( void );
+public:
+	Karen(/* args */);
+	~Karen();
+	void complain( std::string level );
+};
