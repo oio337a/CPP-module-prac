9eafb143c563f5f98238a27c6e61aa6956ea1e8c
oio337a <oio337a@gmail.com>
2023-08-10 23:26:50 +0900

    ex06 clear

--- cpp_module_01/ex05/Harl.hpp
+++ cpp_module_01/ex05/Harl.hpp
@@ -4,14 +4,14 @@
 class Harl
 {
 private:
-  void  debug(void);
-  void  info(void);
-  void  warning(void);
-  void  error(void);
+  void debug(void);
+  void info(void);
+  void warning(void);
+  void error(void);
+
 public:
   Harl(/* args */);
   ~Harl();
 
   void complain(std::string level);
-  
 };
