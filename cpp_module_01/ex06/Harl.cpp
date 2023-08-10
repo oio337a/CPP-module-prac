478c882cbdd9ea71bc61c0a82f7a169f94d1ec7e
oio337a <oio337a@gmail.com>
2023-08-10 23:29:00 +0900

    ex06 clear

--- cpp_module_01/ex06/Harl.cpp
+++ cpp_module_01/ex06/Harl.cpp
@@ -50,13 +50,10 @@ void Harl::complain(std::string level)
   {
   case 0:
     this->debug();
-    break;
   case 1:
     this->info();
-    break;
   case 2:
     this->warning();
-    break;
   case 3:
     this->error();
     break;
