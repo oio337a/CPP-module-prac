38307108efaca173e55790056bd206c97c38ae7c
oio337a <yongmipa@student.42seoul.kr>
2023-08-20 13:27:02 +0900

    ex03 clear

--- cpp_module_01/ex03/HumanB.cpp
+++ cpp_module_01/ex03/HumanB.cpp
@@ -1,6 +1,6 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name)
+HumanB::HumanB(std::string name) : name(name), weapon(NULL)
 {
   std::cout << this->name << " 이 생성되었어요. " << std::endl;
 }
@@ -12,6 +12,10 @@ void HumanB::setWeapon(Weapon &weapon)
 
 void HumanB::attack() const // const 를 붙여줘야 함 (const 함수 안에서는 멤버 변수를 변경할 수 없음)
 {
+  if (!this->weapon)
+  {
+    std::cout << "weapon 이 장착되지 않았습니다." << std::endl;
+  }
   std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
 }
 
