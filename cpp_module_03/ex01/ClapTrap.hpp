b6d4718148b65c656a3769d6ad0d191c1331b020
oio337a <oio337a@gmail.com>
2023-08-24 23:52:38 +0900

    ex01 clear

--- cpp_module_03/ex01/ClapTrap.hpp
+++ cpp_module_03/ex01/ClapTrap.hpp
@@ -3,7 +3,7 @@
 
 class ClapTrap
 {
-private:
+protected:
   std::string _name;
   unsigned int _HP;
   unsigned int _EP;
--- cpp_module_03/ex01/ScavTrap.cpp
+++ cpp_module_03/ex01/ScavTrap.cpp
@@ -5,14 +5,22 @@
  * Orthodox Canonical Form
  */
 
-ScavTrap::ScavTrap() : _name("ScavTrap"), _HP(100), _EP(50), _AD(20)
+ScavTrap::ScavTrap()
 {
   std::cout << "ScavTrap Default constructor called" << std::endl;
+  this->_name = "ScavTrap";
+  this->_HP = 100;
+  this->_EP = 50;
+  this->_AD = 20;
 }
 
-ScavTrap::ScavTrap(const ScavTrap &scavtrap) : _name(scavtrap._name), _HP(scavtrap._HP), _EP(scavtrap._EP), _AD(scavtrap._AD)
+ScavTrap::ScavTrap(const ScavTrap &scavtrap)
 {
   std::cout << "ScavTrap copied " << scavtrap._name << " constructor called!" << std::endl;
+  this->_name = scavtrap._name;
+  this->_HP = scavtrap._HP;
+  this->_AD = scavtrap._AD;
+  this->_EP = scavtrap._EP;
 }
 
 ScavTrap &ScavTrap::operator=(const ScavTrap &scavtrap)
@@ -33,14 +41,18 @@ ScavTrap &ScavTrap::operator=(const ScavTrap &scavtrap)
  * Constructor & Destructor
  */
 
-ScavTrap::ScavTrap(std::string name) : _name(name), _HP(100), _EP(50), _AD(20)
+ScavTrap::ScavTrap(std::string name)
 {
-	std::cout << "ScavTrap " << this->name << " constructor called" << std::endl;
+  this->_name = name;
+  this->_HP = 100;
+  this->_EP = 50;
+  this->_AD = 20;
+	std::cout << "ScavTrap " << this->_name << " constructor called" << std::endl;
 }
 
 ScavTrap::~ScavTrap(void)
 {
-	std::cout << "ScavTrap " << this->name << " destructor is called" << std::endl;
+	std::cout << "ScavTrap " << this->_name << " destructor is called" << std::endl;
 }
 
 /**
@@ -50,12 +62,35 @@ ScavTrap::~ScavTrap(void)
 
 void ScavTrap::guardGate(void)
 {
-  if (!this->_HP || !this->_EP) {
+  if (!this->_HP || !this->_EP)
+  {
     std::cout << "ScavTrap " << this->_name << " point is empty" << std::endl;
     return ;
   }
-  else {
-    std::cout << "ScavTrap " << this->_name << " attack " << target << ", causing " << this->_AD << " points of damage" << std::endl;
+  else
+  {
+		std::cout<< "ScavTrap " << this->_name << " guardgate is called" << std::endl;
     this->_EP--;
+  }
+}
+
+void ScavTrap::attack(const std::string &target)
+{
+  if (this->_HP <= 0)
+  {
+    std::cout << this->_name << " cannot attack, he has 0 HP left!" << std::endl;
+  }
+  else
+  {
+    for (int i = 0; i < (int)this->_AD; i++)
+    {
+      if (this->_EP <= 0)
+      {
+        std::cout << "ScavTrap " << this->_name << " attacks " << target << " causing " << this->_AD << " points of damage! But he has no EP left!" << std::endl;
+        return;
+      }
+      this->_EP--;
+    }
+    std::cout << "ScavTrap " << this->_name << " attacks " << target << " causing " << this->_AD << " points of damage!" << std::endl;
   }
 }
