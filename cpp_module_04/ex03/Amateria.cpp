05ec759375b599a0d0c75faae7a28e68fdc55b76
oio337a <oio337a@gmail.com>
2023-08-27 00:16:36 +0900

    ex03 작성중

---
+++ cpp_module_04/ex03/Amateria.cpp
@@ -0,0 +1,39 @@
+#include "Amateria.hpp"
+
+AMateria::AMateria() : _type("default")
+{
+  std::cout << "AMateria default constructor called" << std::endl;
+}
+
+AMateria::AMateria(std::string const &type) : _type(type)
+{
+  std::cout << "AMateria type constructor called" << std::endl;
+}
+
+AMateria::AMateria(const AMateria &copy) : _type(copy._type)
+{
+  std::cout << "AMateria copy constructor called" << std::endl;
+}
+
+AMateria &AMateria::operator=(const AMateria &copy)
+{
+  std::cout << "AMateria assignation operator called" << std::endl;
+  if (this != &copy)
+    this->_type = copy._type;
+  return (*this);
+}
+
+AMateria::~AMateria()
+{
+  std::cout << "AMateria destructor called" << std::endl;
+}
+
+std::string const &AMateria::getType() const
+{ 
+  return (this->_type);
+}
+
+void AMateria::use(ICharacter &target)
+{
+  // std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std::endl;
+}
